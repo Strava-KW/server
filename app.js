b6bb35037c649f8a5681e72b0e93670f19680dcd
muchsin28 <hab@muchsin.id>
2021-01-24 10:19:14 +0700

    tes wiring

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-require('dotenv').config()
-const express = require("express")
-const app = express()
-const router = require('./routes')
-const errorHandler = require('./middlewares/errorHandling')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const router = require("./routes");
+const errorHandler = require("./middlewares/errorHandling");
+// const cors = require("cors");
+const mongoose = require("mongoose");
+const url =
+  "mongodb+srv://strava:strava123@cluster0.jds7c.mongodb.net/Development?retryWrites=true&w=majority";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+// app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
+app.use(errorHandler);
 
-// const mongoose = require('mongoose')
-// const url = "mongodb+srv://strava:strava123@cluster0.jds7c.mongodb.net/Development?retryWrites=true&w=majority"
-// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(router)
-app.use(errorHandler)
-
-module.exports = app
+module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,89 +1,87 @@
-const User = require('../models/User')
-const { checkPassword } = require('../helpers/bcrypt')
-const { createToken } = require('../helpers/jwt')
+const User = require("../models/User");
+const { checkPassword } = require("../helpers/bcrypt");
+const { createToken } = require("../helpers/jwt");
 
 class UserController {
-    static register(req, res, next) {
-        const user = new User({
-            fullname: req.body.fullname,
-            email: req.body.email,
-            password: req.body.password,
-            history: [],
-            totalRange: 0,
-            role: null,
-            communityId: null
-        })
-        user
-        .save()
-        .then(data => {
-            res.status(201).json({
-                fullname: data.fullname,
-                email: data.email,
-                _id: data._id
-            })
-        })
-        .catch(err => {
-            next(err)
-        })
-    }
+  static register(req, res, next) {
+    console.log(req.body);
+    const user = new User({
+      fullname: req.body.fullname,
+      email: req.body.email,
+      password: req.body.password,
+      history: [],
+      totalRange: 0,
+      role: null,
+      communityId: null,
+    });
+    user
+      .save()
+      .then((data) => {
+        res.status(201).json({
+          fullname: data.fullname,
+          email: data.email,
+          _id: data._id,
+        });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 
-    static login(req, res, next) {
-        User.findOne({email: req.body.email})
-            .exec()
-            .then(result => {
-                if (result) {
-                    console.log("get people")
-                    if (checkPassword(req.body.password, result.password)){
-                        let obj = {
-                            id: result._id,
-                            email: result.email,
-                            role: result.role,
-                            communityId: result.communityId
-                        }
-                        res.status(200).json({access_token: createToken(obj)})
-                        console.log("checked pass")
-                    }
-                    else {
-                        console.log("invalid pass")
-                        throw {
-                            status: 401,
-                            message: "Invalid email/password"
-                        }
-                        
-                    }
-                }
-                else {
-                    throw {
-                        status: 401,
-                        message: "Invalid email/password"
-                    }
-                }
-            })
-            .catch(err => {
-                next(err)
-            })
-    }
+  static login(req, res, next) {
+    User.findOne({ email: req.body.email })
+      .exec()
+      .then((result) => {
+        if (result) {
+          console.log("get people");
+          if (checkPassword(req.body.password, result.password)) {
+            let obj = {
+              id: result._id,
+              email: result.email,
+              role: result.role,
+              communityId: result.communityId,
+            };
+            res.status(200).json({ access_token: createToken(obj) });
+            console.log("checked pass");
+          } else {
+            console.log("invalid pass");
+            throw {
+              status: 401,
+              message: "Invalid email/password",
+            };
+          }
+        } else {
+          throw {
+            status: 401,
+            message: "Invalid email/password",
+          };
+        }
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 
-    // static getAllUser(req, res, next) {
-    //     User.find()
-    //     .exec()
-    //     .then(data => {
-    //         res.json(data)
-    //     })
-    //     .catch(err => {
-    //         res.json(err)
-    //     })
-    // }
+  // static getAllUser(req, res, next) {
+  //     User.find()
+  //     .exec()
+  //     .then(data => {
+  //         res.json(data)
+  //     })
+  //     .catch(err => {
+  //         res.json(err)
+  //     })
+  // }
 
-    // static deleteUser (req, res, next) {
-    //     User.findOneAndDelete({_id: req.params.userId})
-    //         .exec()
-    //         .then(_ => {
-    //             res.status(200).json("deleted")
-    //         })
-    //         .catch(err => {
-    //             next(err)
-    //         })
-    // }
+  // static deleteUser (req, res, next) {
+  //     User.findOneAndDelete({_id: req.params.userId})
+  //         .exec()
+  //         .then(_ => {
+  //             res.status(200).json("deleted")
+  //         })
+  //         .catch(err => {
+  //             next(err)
+  //         })
+  // }
 }
-module.exports = UserController
+module.exports = UserController;
