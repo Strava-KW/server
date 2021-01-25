908e4b66c8ffdb45ce7339fa8c3a46eb79974493
muchsin28 <hab@muchsin.id>
2021-01-25 21:28:10 +0700

    Deploy 3

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./routes");
-const cors = require('cors')
 const errorHandler = require("./middlewares/errorHandling");
+const cors = require("cors");
 const mongoose = require("mongoose");
-const url = process.env.MONGODB_URL || "mongodb://localhost:27017/Runator";
+const url =
+  "mongodb+srv://strava:strava123@cluster0.jds7c.mongodb.net/Development?retryWrites=true&w=majority";
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(cors());
 app.use(express.json());
