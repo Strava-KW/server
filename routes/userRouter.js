12f53b52cc2b59bc3aee635e814a161366bad7fb
agnespravida <agnespravida27@gmail.com>
2021-01-25 18:49:22 +0700

    google login

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,6 +4,7 @@ const UserController = require("../controllers/userController");
 
 router.post("/login", UserController.login);
 router.post("/register", UserController.register);
+router.post("/googleLogin", UserController.googleLogin)
 
 // router.get('/', UserController.getAllUser)
 // router.delete('/:userId', UserController.deleteUser)
