3be19345fce43e729718676d0e4df14da11bbc68
muchsin28 <hab@muchsin.id>
2021-01-27 17:57:59 +0700

    Deploy Google App Engine

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const http = require("http")
-const app = require("./app")
-const PORT = process.env.PORT || 3000
+const http = require("http");
+const app = require("./app");
+const PORT = process.env.PORT || 8080;
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log("App listening on port " + PORT)
-})
+  console.log("App listening on port " + PORT);
+});
